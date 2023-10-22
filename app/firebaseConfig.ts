79587a719c2a7ccd3ebf6b05c7c81f90f50d1691
Glebunya1234 <glebbuckiy@gmail.com>
2023-10-22 15:51:34 +0300

    auth is completed, but 50%

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,25 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { get } from "http";
-import { getAuth } from "firebase/auth/cordova";
+import { getAuth } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJ32NSaQMb8cpyHmJR27jX3X8DZ2YgaKE",
-  authDomain: "notecloud-4cea9.firebaseapp.com",
-  projectId: "notecloud-4cea9",
-  storageBucket: "notecloud-4cea9.appspot.com",
-  messagingSenderId: "391946937941",
-  appId: "1:391946937941:web:9dae7431386045835b56dc",
-  measurementId: "G-3NGG3JKPC4"
+  apiKey: "AIzaSyAteFGzuFOnqZEi6KnubDydS4Y5-VEFFHQ",
+  authDomain: "notes-2889f.firebaseapp.com",
+  projectId: "notes-2889f",
+  storageBucket: "notes-2889f.appspot.com",
+  messagingSenderId: "91818343889",
+  appId: "1:91818343889:web:0c190710ca8098ddaab4f0",
+  measurementId: "G-TSYE4LE0N8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
- 
+
 export const authh = getAuth(app)
