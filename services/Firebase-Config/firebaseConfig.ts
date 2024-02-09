996fd7419d8fcdcc5aae6f01fc01718e2f9fbaee
Glebunya1234 <glebbuckiy@gmail.com>
2024-02-09 18:25:18 +0200

    Update

--- services/Firebase-Config/firebaseConfig.ts
+++ services/Firebase-Config/firebaseConfig.ts
@@ -3,21 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { doc, getFirestore, onSnapshot, setDoc } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
-import { Keys } from "../../utils/Keys";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: Keys.APIKEY,
-  authDomain: Keys.AUTHDOMAIN,
-  projectId: Keys.PROJECTID,
-  storageBucket: Keys.STORAGEBUCKET,
-  messagingSenderId: Keys.MESSAGINGSENDERID,
-  appId: Keys.APPID,
-  measurementId: Keys.MEASUREMENTID
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
