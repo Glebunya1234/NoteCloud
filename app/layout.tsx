1c0e9ec7af54e3f5b5135e6b6f0ac05ed9199da9
Glebuska <135132499+Glebunya1234@users.noreply.github.com>
2024-06-01 17:59:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     title: "NoteCloud",
     description: "The project is a web application designed for creating, editing, and deleting scheduled tasks.",
     type: "website",
-    images: "assets/Prew.png",
+    images: "https://github.com/Glebunya1234/NoteCloud/blob/master/assets/Prew.png?raw=true",
     url: "https://note-cloud-five.vercel.app",
   },
 };
